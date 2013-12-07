5a5a50700d7549b4d6af8fd1007667d479abacc9
Shell Yan <shellyan@Shells-MacBook-Pro.local>
2013-12-07 17:18:14 -0600

    new

--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to git clean -fdx&#8217;s documentation!"],objects:{},titleterms:{git:0,welcom:0,indic:0,clean:0,tabl:0,fdx:0,document:0}})
