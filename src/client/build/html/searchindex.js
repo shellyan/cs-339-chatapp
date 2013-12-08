6a631ac9f56e6ef5d024c614e1309c177c3badc9
Shell Yan <shellyan@Shells-MacBook-Pro.local>
2013-12-08 10:49:35 -0600

    merge sphinx and client2

---
+++ src/client/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,chatapp:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to  cs-339-chatapp &#8216;s documentation!"],objnames:{},filenames:["documentation"]})
