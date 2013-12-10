bafc6f18acf08d5eb45503bb8baaf45914cedafb
Shell Yan <shellyan@Shells-MacBook-Pro.local>
2013-12-09 18:22:30 -0600

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 /* 
-This is the javascript used in conjunction with index.html and style.css
+This is the javascript used to add functionality to our PeerJS app.
 */
 
-var username = "<span class = 'username' =>You: </span>"; //your 'name; output to screen next to your messages
+var username = "<span class = 'username' =>You: </span>"; //your 'name'; output to screen next to your messages
 
-var allConns = new Array();
-var allPeers = new Array();
+var allConns = new Array(); //list of all current connections
+var allPeers = new Array(); //list of all current peers of allConns (needed so we do not have duplicate conncetions)
 
 // Connect to PeerJS, server assigns ID
 var peer = new Peer({key: 'lwjd5qra8257b9', debug: true});
-//var peer = new Peer({host: '10.23.54.117', port: 49445, debug: true});  
 		peer.on('open', function(id){
 		$('#pid').text(id);
 		allPeers.push(id);
@@ -18,14 +17,13 @@ var peer = new Peer({key: 'lwjd5qra8257b9', debug: true});
 // Await connections from others
 peer.on('connection', connect);
 
+//Prompts the user that their twitter discussion will be removed if they leave the page.
 window.onbeforeunload = function(){
     if($('#connect').is(":disabled")==true&&$('#broadcast').is(":disabled")==true){
         deleteTweet($('#pid').text());
-        return 'The topic you created on twitter will be deleted. '
+        return null;
     }
-
-    return null
-
+    return null;
 }
 
 // Connect to a peer
@@ -36,20 +34,49 @@ $('#connect').click(function(){
     connect(c); //sets up a connection between the peers
   });
   c.on('error', function(err){ alert(err) });
-
-
 });
+
+
+
+
+
+function searchTopic(id){
+    request = $.ajax({
+    url: 'http://cs-339-chatapp.herokuapp.com/search',
+//    url: 'http://127.0.0.1:5000/search',
+    type: "post",
+    data: {
+        "content": id
+    }
+    });
+    request.done(function (response, textStatus, jqXHR){
+        // log a message to the console
+//        alert("Hooray, posted to twitter!");
+        $('#chat_title').text(response)
+    });
+
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // log the error to the console
+
+        alert(
+            "The following error occured: "+
+            textStatus, errorThrown
+        );
+    });
+}
 
 
 // Connects our peers
 function connect(c) {
     var conn = c;
     $('#container').append('Now chatting with ' + conn.peer + '<br>');
-    //diable connect button once connected
-    $('#connect').prop('disabled', true);
-
+    searchTopic(conn.peer);
 	notifyOthers(c.peer);
 	addConns(conn);
+	
+	//disable connect button once connected
+    $('#broadcast_radio').prop('disabled', true);
 	
 	var othername = "<span class = 'othername' => <font color=\"green\">" + conn.peer + ":</font> </span>";
 	
@@ -58,6 +85,7 @@ function connect(c) {
 	
 	var array = data.split(" ");
 	
+	//different commands
 	switch(array[0])
 	{
 		case "/add":
@@ -92,14 +120,7 @@ function connect(c) {
 	});
  }
 
-
-
- function processCommand(command, data){
-
- }
- 
- 
- //tells others to add to call.
+ //tells others to add your new peer to their list of connections.
  function notifyOthers(peer) {
 	for(var i = 0; i < allConns.length; i++)
 	{
@@ -141,6 +162,7 @@ function connect(c) {
  }
 
 
+ //deletes the tweet once the broadcaster leaves the room
 function deleteTweet(id){
         console.log('trying to delete '+ id);
         request = $.ajax({
@@ -181,6 +203,7 @@ $(document).ready(function() {
 
     	// Connect to a peer
     $('#connect').click(function(){
+	  
       var c = peer.connect($('#rid').val());
 	  $('#rid').val("");
       c.on('open', function(){
@@ -197,6 +220,7 @@ $(document).ready(function() {
 	  
 	  var array = msg.split(" ");
 	  
+	  //various commands
 	  switch(array[0]) 
 	  { 
 			case "/nick":
